a4ef280625cdd41fe271737c07e97d89775e8f00
jdcepeda76 <jdcepeda76@misena.edu.co>
2020-10-20 21:12:55 -0500

    Se realizo el cambio de base de datos en servidor

--- conexion.php
+++ conexion.php
@@ -1,9 +1,9 @@
 <?php 
 
-	$host = 'localhost';
-	$user = 'root';
-	$password = '';
-	$db = 'jlm';
+	$host = 'u3r5w4ayhxzdrw87.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+	$user = 'ci9uudxph91zpamm';
+	$password = 'uy4ca2zktmvnpsnt';
+	$db = 'zx2oanmxntl5kzh1';
 
 
 	$conection = @mysqli_connect($host,$user,$password,$db);
