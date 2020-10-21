5678e5d58bcf05bd931080bea8d3b3a2a1928196
jdcepeda76 <jdcepeda76@misena.edu.co>
2020-10-20 19:48:03 -0500

    Agregado el proyecto a GitHub

---
+++ sistema/includes/header.php
@@ -0,0 +1,24 @@
+	<?php 
+
+	if (empty($_SESSION['active'])) 
+	{
+		header('location: ../');
+	}
+	 ?>
+	<header>
+		<div class="header">
+			<a href="#" class="btnMenu"><i class="fas fa-bars"></i></a>
+			<h1>JLM || WORKSHOP</h1>
+			<div class="optionsBar">
+				<span class="user"><?php echo $_SESSION['nombre'].' - ' .$_SESSION['rol']; ?></span>
+				<img class="photouser" src="img/user.png" alt="Usuario">
+				<a href="salir.php"><img class="close" src="img/salir.png" alt="Salir del sistema" title="Salir"></a>
+			</div>
+		</div>
+		<?php include "nav.php"; ?>
+	</header>
+
+	<div class="modal">
+		<div class="bodyModal">
+		</div>	
+	</div>
